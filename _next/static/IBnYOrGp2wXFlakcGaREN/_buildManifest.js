851819bfa43d60f8bbfb201c6980bbdec207bca7
marcos <marcosarguellesrivera@gmail.com>
2026-02-10 21:58:26 +0100

    Updates

---
+++ _next/static/IBnYOrGp2wXFlakcGaREN/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/b5d145c6e8740668.js"
+  ],
+  "/_error": [
+    "static/chunks/cb5ffe228c53f90f.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/api/hello"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/b5d145c6e8740668.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/4fcdc8e201175612.js",
+  "static/chunks/4555de9f635996bc.js",
+  "static/chunks/d728be3c1c488e3c.js",
+  "static/chunks/877cdad0a5129077.js",
+  "static/chunks/turbopack-40cb891eeb51c424.js"
+])
